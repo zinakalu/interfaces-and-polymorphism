6ce9dea4195efdd46fc3d468f83b554fc418c047
zinakalu <zina.kalu@gmail.com>
2023-05-11 09:16:00 -0400

    additional methods

--- Report.java
+++ Report.java
@@ -1,18 +1,31 @@
 abstract class Report{
     void runReport(){
-        //set up Report
+       System.out.println("Setting up the report...");
     }
     void printReport(){
-        //generic printing
+        System.out.println("Printing the report...");
     }
 }
 
 class BuzzwordsReport extends Report{
     @Override
     void runReport(){
-        super.runReport();
-        buzzwordCompliance();
-        printReport();
+        super.runReport(); //invoking the runReport() method of the superclass (Report)
+        buzzwordCompliance(); //specific operation defined in the BuzzwordsReport class
+        printReport(); //invoking the printReport() method of the superclass (Report)
     }
-    void buzzwordCompliance(){...}
+    void buzzwordCompliance(){
+        System.out.println("Complying to the buzzwords");
+    }
 }
+
+public class Main{
+    public static void main(String[] args){
+        BuzzwordsReport buzzwordsReport = new BuzzwordsReport();
+        buzzwordsReport.runReport();
+    }
+}
+
+//you can invoke the superclass method('runReport()') and combine its
+//functionality with the specific operations ('buzzwordCompliance()')defined
+//in the subclass ('BuzzwordsReport').
